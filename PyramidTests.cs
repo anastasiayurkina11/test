5a17dc62bb7e93a9d448b66ff49744a33cfd9991
anastasiayurkina11 <106163636+anastasiayurkina11@users.noreply.github.com>
2023-09-26 14:53:32 +0300

    Add files via upload

---
+++ PyramidTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class PyramidTests
+    {
+        [TestMethod()]
+        [DataRow(4, 6, 8, 64)]
+        [DataRow(10, 12, 14, 560)]
+        [DataRow(-20, 23, 14, 0)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateVolumeTest(double baseLength, double baseWidth, double height, double expectedVolume)
+        {
+            // Arrange
+            Pyramid pyramid = new Pyramid(baseLength, baseWidth, height);
+
+            // Act
+            double actualVolume = pyramid.CalculateVolume();
+
+            // Assert
+            Assert.AreEqual(expectedVolume, actualVolume);
+        }
+    }
+}
